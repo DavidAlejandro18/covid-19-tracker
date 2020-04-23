34224cc049744c7b8f8b3e6b753399d952a4502a
David Tovar <alextovar146@gmail.com>
2020-04-22 19:51:39 -0500

    Se agregan los cambios del log anterior a la carpeta public para el deploy en Firebase

--- public/series.js
+++ public/series.js
@@ -6,6 +6,12 @@ var selectPaises = document.getElementById('paises');
 var selectProvincias = document.getElementById('provincias');
 var btnBuscar = document.getElementById('buscar');
 
+// Obtener el contenedor del preloader
+var preloader = document.getElementById('preloader');
+
+// Obtener el contenedor de la gráfica
+var chart = document.getElementById('myChart');
+
 // Se agregan todos los paises al selectPaises para mostrar su información
 fetch(opcionesPaises)
     .then(response => response.json())
@@ -68,12 +74,14 @@ btnBuscar.addEventListener("click", function () {
     let date = [];
     let value = [];
 
+    preloader.style.display = 'block';
+    chart.style.display = 'none'
+
     series()
         .then((data) => {
             data.data.map((paises) => {
                 if (paises["Country/Region"] == selectedOptionPaises.text) {
                     if (paises["Province/State"] == selectedOptionProvincias.text) {
-                        //console.log(paises['TimeSeries']);
                         serieTiempo.push(paises['TimeSeries']);
                     }
                 }
@@ -83,7 +91,6 @@ btnBuscar.addEventListener("click", function () {
                 date.push(valor.date);
                 value.push(valor.value);
             });
-            //console.log(date);
             
             if (window.bar != undefined) {
                 window.bar.destroy();
@@ -110,12 +117,26 @@ btnBuscar.addEventListener("click", function () {
                     scales: {
                         yAxes: [{
                             ticks: {
-                                beginAtZero: true
+                                beginAtZero: true,
+                                callback: function (value) {
+                                    return formatNumber(value)
+                                }
                             }
                         }]
+                    },
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem, chart){
+                                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                                return datasetLabel + ': ' + formatNumber(tooltipItem.yLabel);
+                            }
+                        }
                     }
                 }
             });
+
+            chart.style.display = 'block';
+            preloader.style.display = 'none';
         })
         .catch((err) => console.error(err));
 });
@@ -125,30 +146,6 @@ async function series() {
     return await response.json();
 }
 
-/* var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6', 'Dia 7', 'Dia 8', 'Dia 9', 'Dia 10', 'Dia 11', 'Dia 12', 'Dia 13', 'Dia 14', 'Dia 15', 'Dia 16', 'Dia 17', 'Dia 18', 'Dia 19', 'Dia 20', 'Dia 21', 'Dia 22', 'Dia 23', 'Dia 24', 'Dia 25', 'Dia 26', 'Dia 27', 'Dia 28', 'Dia 29', 'Dia 30', 'Dia 31'],
-        datasets: [{
-            label: 'Número de contagios',
-            data: [1, 1, 3, 5, 7, 8, 10, 12, 19, 23, 45, 52, 53, 60, 72, 79, 83, 85, 92, 93, 100, 102, 105, 109, 120, 130, 175, 250, 302, 308, 398],
-            backgroundColor: [
-                'rgba(44, 62, 80, 0.2)'
-            ],
-            borderColor: [
-                'rgba(28, 40, 51, 1)'
-            ],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-}); */
+const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
